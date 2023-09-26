62bd7401a67b61f7cd629c74b0341967c1b44e34
Walter Coayla <walter.coayla@gmail.com>
2023-09-25 20:11:18 -0500

    Act 250923 - Plantilla AdminLTE

--- controlador/CtrlPrincipal.php
+++ controlador/CtrlPrincipal.php
@@ -1,23 +1,28 @@
 <?php
+session_start();
 require_once './core/Controlador.php';
 require_once './modelo/Oficina.php';
 
 class CtrlPrincipal extends Controlador {
     public function index(){
         #echo "Hola mundo";
-        $obj = new Oficina();
-        $data = $obj->mostrar();
+        /* $obj = new Oficina();
+        $data = $obj->mostrar(); */
 
         # var_dump($data);exit;
-
+        $_SESSION['menu']=$this->getMenu();
         $datos = [
             'menu'=>$this->getMenu(),
             'titulo'=>'Sistema IES.',
             'usuario'=>'Walter',
-            'datos'=>$data['data']
+            // 'datos'=>$data['data']
         ];
 
-        $this->mostrar('home.php',$datos);
+        $home = $this->mostrar('home.php',$datos,true);
+        $datos = [
+            'contenido'=>$home
+        ];
+        $this->mostrar('plantilla/home.php',$datos);
 
     }
 
--- vistas/home.php
+++ vistas/home.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cuarto</title>
-</head>
-<body>
+
     <h1><?=isset($titulo)?$titulo:'';?></h1>
-    <h3>Bienvenido: <?=isset($usuario)?$usuario:'';?></h3>
+    <h3>Bienvenido al sistema de ies jcm: <?=isset($usuario)?$usuario:'';?></h3>
 
 
     <ul>
@@ -24,5 +17,3 @@
 ?>
 
     </ul>
-</body>
-</html>
